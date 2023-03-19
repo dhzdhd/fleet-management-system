c18c3445228de3b4b86312345f919028f00669c6
dhzdhd <ragas5432100@gmail.com>
2023-03-19 19:16:48 +0530

    Create sample connection to oracle

--- app/app.py
+++ app/app.py
@@ -2,13 +2,18 @@ import sys
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
+from . import backend as b
 
 
 def setup():
     app = QGuiApplication(sys.argv)
 
+    db = b.Database()
+    db.execute()
+    db.close()
+
     engine = QQmlApplicationEngine()
     engine.quit.connect(app.quit)
-    engine.load('app/qml/main.qml')
+    engine.load("app/qml/main.qml")
 
     sys.exit(app.exec())
---
+++ app/backend.py
@@ -0,0 +1,26 @@
+import oracledb as odb
+
+
+class Database:
+    def __init__(self) -> None:
+        # Run lsnrctl status to get values
+        self.conn: odb.Connection = odb.connect(
+            user="system",
+            password="",
+            port=1521,
+            host="localhost",
+            service_name="deep",
+        )
+
+    def close(self) -> None:
+        self.conn.close()
+
+    def execute(self) -> None:
+        with self.conn.cursor() as cursor:
+            cursor.execute("CREATE TABLE a(b NUMBER)")
+            cursor.execute("INSERT INTO a VALUES(:1)", (1,))
+
+            for row in cursor.execute("SELECT * FROM a"):
+                print(row)
+
+            cursor.execute("DROP TABLE a")
